7a588403ec7a255341330792674bc184796c7bf2
sahlbakshi <sahlaajazbakshi@gmail.com>
2025-08-13 20:09:56 -0400

    better import

--- utils/audio.py
+++ utils/audio.py
@@ -1,7 +1,7 @@
-import io
+from io import BytesIO
 from mutagen.mp3 import MP3
 
 def get_audio_duration_ms(audio_bytes: bytes) -> int:
-    f = io.BytesIO(audio_bytes)
+    f = BytesIO(audio_bytes)
     return int(MP3(f).info.length * 1000)
     
