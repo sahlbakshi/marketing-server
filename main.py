458ef94ceeff75ca3abb6b9738b10ef4861c04d1
sahlbakshi <sahlaajazbakshi@gmail.com>
2025-08-16 03:17:07 -0400

    feat: added cors

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from routes import fake_text_video
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(fake_text_video.router)
 
